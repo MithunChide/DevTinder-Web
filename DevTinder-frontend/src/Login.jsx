35151770117c00e13209222be1055f81dc8d23d0
MithunChide <mithunchide03@gmail.com>
2026-06-20 23:57:37 +0530

    Login using the post api

--- DevTinder-frontend/src/Login.jsx
+++ DevTinder-frontend/src/Login.jsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+    const [emailId, setEmailId] = useState("mithun@gmail.com");
+    const [password, setPassword] = useState("Mithun@143");
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post("http://localhost:7777/login", {
+                emailId,
+                password
+            }, 
+            {
+                withCredentials: true
+            })
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
   return (
-    <div>Login</div>
+        <div className="flex justify-center my-6">
+             <div className="card bg-neutral w-96 shadow-sm">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Login</h2>
+                    <div className="form-control w-full max-w-xs my-4">
+                        <label className="label">
+                            <span className="label-text">Email Address:</span>
+                        </label>
+                        <input 
+                            type="text" 
+                            value={emailId}
+                            placeholder="Email Address..." 
+                            className="input input-bordered w-full max-w-xs my-2" 
+                            onChange={(e) => setEmailId(e.target.value)}
+                        />
+                        <label className="label">
+                            <span className="label-text-alt">Password:</span>
+                        </label>
+                        <input 
+                            type="text" 
+                            value={password}
+                            placeholder="Password..." 
+                            className="input input-bordered w-full max-w-xs my-2" 
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        </div>
+
+                    <div className="card-actions justify-center">
+                    <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
   )
 }
 
