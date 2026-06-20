c8ffbf98774efaa037ded8059b559a834e50c318
MithunChide <mithunchide03@gmail.com>
2026-06-20 17:53:14 +0530

    BouserRouter configuration

--- DevTinder-frontend/src/App.jsx
+++ DevTinder-frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
 
 function App() {
 
   return (
-   <div>
-    <h1>Hello World</h1>
-   </div>
+   <>
+    <BrowserRouter basename="/">
+      <Routes>
+          <Route path="/" element= {<Body/>}> 
+              <Route path="/login" element= {<Login/>}/>
+              <Route path="/profile" element= {<Profile/>}/>
+
+          </Route> 
+      </Routes>
+    </BrowserRouter>
+
+   </>
   )
 }
 
