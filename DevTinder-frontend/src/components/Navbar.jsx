a209fd1b6f8732722c8adb76a9e58598c1d5e7a2
MithunChide <mithunchide03@gmail.com>
2026-06-21 01:13:53 +0530

    constant file added

--- DevTinder-frontend/src/components/Navbar.jsx
+++ DevTinder-frontend/src/components/Navbar.jsx
@@ -10,31 +10,33 @@ const Navbar = () => {
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">DevTinder</a>
   </div>
-  { user && (<div className="flex gap-2 mx-6 items-center">
-    <div className="px-2"> Welcome, {user.data.firstName}</div>
-    <div className="dropdown dropdown-end">
-    
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="user photo"
-            src={user.data.photoUrl} />
+  { user && (
+      <div className="flex gap-2 mx-6 items-center">
+      <div className="px-2"> Welcome, {user.data.firstName}</div>
+      <div className="dropdown dropdown-end">
+      
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="user photo"
+              src={user.data.photoUrl} />
+          </div>
         </div>
+        <ul
+          tabIndex="-1"
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+          <li>
+            <a className="justify-between">
+              Profile
+              <span className="badge">New</span>
+            </a>
+          </li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
       </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>)}
+      </div>
+  )}
   
 </div>
   )
